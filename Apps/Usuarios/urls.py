b9f1ecebf691be4e52a46b11d8e0ad0add22728e
Edison Felipe Mamián Cerón <edison.mamian@correounivalle.edu.co>
2020-10-21 21:27:30 -0500

    correccion advertencia urls con id

--- Apps/Usuarios/urls.py
+++ Apps/Usuarios/urls.py
@@ -5,8 +5,8 @@ from Apps.Usuarios.views import *
 app_name = 'Usuarios'
 urlpatterns = [
     path ('roles/crear/', CrearRol.as_view(), name = 'crear_rol'),
-    path ('roles/editar/(?P<pk>\d+)/', ActualizarRol.as_view(), name = 'editar_rol'),
+    path ('roles/editar/<int:rol_id>/', ActualizarRol.as_view(), name = 'editar_rol'),
     path ('crear/', CrearUsuario.as_view(), name = 'crear'),
     path ('listar/', ListarUsuario.as_view(), name = 'listar'),
-    path ('editar/(?P<pk>\d+)/', ActualizarUsuario.as_view(), name = 'editar'),
+    path ('editar/<int:usuario_id>/', ActualizarUsuario.as_view(), name = 'editar'),
 ]
